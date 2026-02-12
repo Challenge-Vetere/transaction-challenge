55fa74b8bf1fc0b355415146d2b27658f16afb94
Giuli <giulianavetere98@gmail.com>
2026-02-12 13:01:09 -0300

    initial commit

---
+++ src/test/java/com/challenge/transaction_challenge/TransactionChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.transaction_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
